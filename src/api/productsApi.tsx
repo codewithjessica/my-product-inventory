a2de65c0ce99856fd072f85f515c1b7524d277fb
“Jessica <codewithjessica@outlook.com>
2024-03-12 10:14:15 -0400

    update NavBar and create NavButton

---
+++ src/api/productsApi.tsx
@@ -0,0 +1,6 @@
+// import axios from " axios ";
+
+// // Get all products
+// export const getAllProducts = () => {
+//     axios.get("https://fakestoreapi.com/products")
+// }
---
+++ src/components /NavButton.tsx
@@ -0,0 +1,20 @@
+import Button from "@mui/material/Button";
+
+interface NavButtonProps {
+    name: string;
+    link: string;
+}
+
+const NavButton = ({ name, link }: NavButtonProps) => {
+  return (
+    <Button 
+    href={link}
+    key={name}
+    sx={{ my:2, color: "white", display: "block"}}
+    >
+        {name}
+    </Button>
+  )
+}
+
+export default NavButton
