9166f6da4ec7f777c63c4ca051ff3440ac0b3edc
“Jessica <codewithjessica@outlook.com>
2024-03-19 10:13:12 -0400

    create productApi.ts

---
+++ src/api/productsApi.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+// CRUD
+
+// GET;
+// GET all products
+
+const PRODUCT_ENDPOINT = import.meta.env.VITE_BASE_URL;
+
+export const getAllProducts = () => axios.get(PRODUCT_ENDPOINT);
+
+// POST;
+// Create a new product
+
+interface ProductType {
+  name: string;
+  email: string;
+  gitHubUsername: string;
+  blogUrl: string;
+}
+
+export const createProduct = (productData: ProductType) =>
+  axios.post(PRODUCT_ENDPOINT, productData);
+
+// PUT / PATCH;
+
+// DELETE;
+export const deleteProduct = (id: string) => axios.delete(`${API_URL}/${id}`);
--- src/api/productsApi.tsx
+++
@@ -1,6 +0,0 @@
-// import axios from " axios ";
-
-// // Get all products
-// export const getAllProducts = () => {
-//     axios.get("https://fakestoreapi.com/products")
-// }
