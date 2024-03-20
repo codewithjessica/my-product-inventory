578eb0c14ea19ac8f6fd4c7257a2dfd45cab4d8e
“Jessica <codewithjessica@outlook.com>
2024-03-20 10:11:52 -0400

    add .env & edit Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,56 @@
 import ProductCard from "../components /ProductCard";
+import { useState, useEffect } from "react";
 import tea from "../assets/tea.jpg";
-import products from "../data/products.json";
+// import products from "../data/products.json";
 import "./Home.css";
 
+// const Home = () => {
+//   const productsArray = products.products.data.items;
+//   return (
+//     <div className="productContainer">
+//       {productsArray.map((product) => (
+//         <ProductCard
+//           image={tea}
+//           name={product.name}
+//           description={product.description}
+//           price={product.price}
+//           rating={product.rating}
+//         />
+//       ))}
+//     </div>
+//   );
+// };
+
 const Home = () => {
-  const productsArray = products.products.data.items;
+  const [products, setProducts] = useState<ProductType[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await getAllProducts();
+        console.log(response.data.data);
+        setProducts(response.data.data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
-    <div className="productContainer">
-      {productsArray.map((product) => (
+    <>
+      {products.map((product) => (
         <ProductCard
-          image={tea}
-          name={product.name}
+          key={product._id}
+          _id={product._id}
+          image={product.image}
+          title={product.title}
           description={product.description}
           price={product.price}
-          rating={product.rating}
         />
       ))}
-    </div>
+    </>
   );
 };
 
